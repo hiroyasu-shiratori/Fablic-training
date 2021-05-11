7f16c6e4a6d12f2ea5f95b99632d1353fec4165e
hiroyasu-shiratori <ts-hiroyas.shiratori@rakuten.com>
2021-05-11 14:02:14 +0900

    fix 見出しをHashから直書きに変更

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,5 @@
 class TasksController < ApplicationController
   def index
     @tasks = Task.all
-
-    keys = @tasks.column_names
-    vals = ['No', 'タスク名', '詳細', 'ステータス', '優先度', '期限', '作成時間', '更新時間']
-
-    @heading = Hash[keys.zip(vals)]
-    @keys = keys
   end
 end
