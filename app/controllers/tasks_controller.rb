9731f12301b7cbb24b374f8b988af3434511381b
hiroyasu-shiratori <ts-hiroyas.shiratori@rakuten.com>
2021-05-13 14:56:03 +0900

    add createアクションでDBに値の保存

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,4 +6,14 @@ class TasksController < ApplicationController
   def new
     @task = Task.new
   end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to root_path
+  end
+
+  def task_params
+    params.require(:task).permit(:title, :description, :status, :priority, :deadline)
+  end
 end
