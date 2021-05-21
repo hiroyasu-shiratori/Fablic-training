ae4ba1d6485735c742a2f1d56a2246f03c1a7a78
hiroyasu-shiratori <ts-hiroyas.shiratori@rakuten.com>
2021-05-21 10:39:15 +0900

    add add タスクの編集のテストを作成

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -1,10 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe 'Tasks', type: :system do
-  # before do
-  #   @user = User.create!(name: 'いとう')
-  # end
-
   it 'Automatically create tasks with JS' do
     # Taskの新規作成画面を開く
     visit new_task_path
@@ -21,16 +17,33 @@ RSpec.describe 'Tasks', type: :system do
     # 優先度を高に設定
     find('#form_priority').find("option[value='高']").select_option
 
-    # 期限に2023/01/01を入力
+    # 期限に2023/01/01 17:28を入力
     fill_in 'form_deadline', with: '002023-01-01-17-28'
 
     # 更新実行
     click_button 'form_submit'
 
-    # # 正しく更新されていること（＝画面の表示が正しいこと）を検証する
+    # 正しく更新されていること（＝画面の表示が正しいこと）を検証する
     expect(page).to have_content 'test'
     expect(page).to have_content '進行中'
     expect(page).to have_content '高'
     expect(page).to have_content '2023-01-01 17:28:00 UTC'
+  end
+
+  it 'Automatically update tasks with JS' do
+    # 編集前のTaskを作成
+    @task = Task.create!(title: '編集前')
+
+    # Taskの編集画面を開く
+    visit edit_task_path(@task.id)
+
+    # タイトルに編集後を入力
+    fill_in 'form_title', with: '編集後'
+
+    # 更新実行
+    click_button 'form_submit'
+
+    # 正しく更新されていること（＝画面の表示が正しいこと）を検証する
+    expect(page).to have_content '編集後'
   end
 end
