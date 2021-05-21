8490e1f7102020690975b8b016a0b2631c201b40
hiroyasu-shiratori <ts-hiroyas.shiratori@rakuten.com>
2021-05-21 14:42:49 +0900

    add `タスクの新規登録`のテストの実装

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'Tasks', type: :system do
-  describe 'タスクの一覧' do
+  describe '`タスクの一覧`のテスト' do
     before do
       # 確認用のタスクを作成
       Task.create(title: 'タスク1')
@@ -27,34 +27,41 @@ RSpec.describe 'Tasks', type: :system do
     end
   end
 
-  # it 'Automatically create tasks with JS' do
-  #   # タスクの新規作成画面を開く
-  #   visit new_task_path
+  describe '`タスクの新規登録`のテスト' do
+    before do
+      # タスクの一覧へ移動
+      visit new_task_path
+    end
 
-  #   # タイトルにtestを入力
-  #   fill_in 'form_title', with: 'test'
+    it 'ページタイトルの確認' do
+      expect(page).to have_title 'タスクの新規登録'
+    end
 
-  #   # 説明にタスクの新規作成テストを入力
-  #   fill_in 'form_description', with: 'タスクの新規作成テスト'
+    it 'タスクが登録できるか確認' do
+      # フォームに値を入れる
+      fill_in 'form_title', with: '新規登録テスト'
+      fill_in 'form_description', with: 'タスクの新規登録テスト'
+      find('#form_status').find("option[value='進行中']").select_option
+      find('#form_priority').find("option[value='高']").select_option
+      fill_in 'form_deadline', with: '002023-01-01-17-28'
+      # タスクの登録ボタン
+      click_button 'form_submit'
+      # 遷移先のタイトルを確認
+      expect(page).to have_title 'タスクの一覧'
+      # 値が追加されている確認
+      expect(page).to have_content '新規登録テスト'
+      expect(page).to have_content '進行中'
+      expect(page).to have_content '高'
+      expect(page).to have_content '2023-01-01 17:28:00 UTC'
+    end
 
-  #   # ステータスを進行中に設定
-  #   find('#form_status').find("option[value='進行中']").select_option
-
-  #   # 優先度を高に設定
-  #   find('#form_priority').find("option[value='高']").select_option
-
-  #   # 期限に2023/01/01 17:28を入力
-  #   fill_in 'form_deadline', with: '002023-01-01-17-28'
-
-  #   # 更新実行
-  #   click_button 'form_submit'
-
-  #   # 正しく更新されていること（＝画面の表示が正しいこと）を検証する
-  #   expect(page).to have_content 'test'
-  #   expect(page).to have_content '進行中'
-  #   expect(page).to have_content '高'
-  #   expect(page).to have_content '2023-01-01 17:28:00 UTC'
-  # end
+    it '入力必須の確認' do
+      # タスクの登録ボタン
+      click_button 'form_submit'
+      # ページが遷移していない事を確認
+      expect(page).to have_title 'タスクの新規登録'
+    end
+  end
 
   # it 'Automatically update tasks with JS' do
   #   # 編集前のタスクを作成
