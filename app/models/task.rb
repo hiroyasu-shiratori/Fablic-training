c7f02516f9c6d93f2b66306669febba14142b8f9
hiroyasu-shiratori <ts-hiroyas.shiratori@rakuten.com>
2021-05-25 17:12:18 +0900

    add descriptionのバリデーションテストを実装

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,4 +2,5 @@
 
 class Task < ApplicationRecord
   validates :title, presence: true, length: { maximum: 100 }
+  validates :description, length: { maximum: 1000 }
 end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -28,37 +28,48 @@ RSpec.describe Task, type: :model do
         expect(task.errors.full_messages).to match_array("Title can't be blank")
       end
     end
+  end
 
-    context '`title`が空白のみの場合、無効' do
-      let(:title) { '' }
+  describe '`description`のバリデーションをテスト' do
+    let(:task) { build(:task, description: description) }
+
+    context '`description`が1000文字の場合、有効' do
+      let(:description) { 'A' * 1000 }
+      it do
+        expect(task).to be_valid
+      end
+    end
+
+    context '`description`が1001文字の場合、無効' do
+      let(:description) { 'A' * 1001 }
       it do
         expect(task).to_not be_valid
-        expect(task.errors.full_messages).to match_array("Title can't be blank")
+        expect(task.errors.full_messages).to match_array('Description is too long (maximum is 1000 characters)')
+      end
+    end
+
+    context '`description`が0文字の場合、有効' do
+      let(:description) { 'A' * 0 }
+      it do
+        expect(task).to be_valid
       end
     end
   end
 
-  describe '`description`のバリデーションをテスト' do
-    it '`description`が1000文字の場合、有効'
-    it '`description`が1001文字の場合、無効'
-    it '`description`が0文字の場合、有効'
-  end
-
   describe '`status`のバリデーションをテスト' do
-    it '`status`が10文字の場合、有効'
-    it '`status`が11文字の場合、無効'
-    it '`status`が0文字の場合、無効'
-    it '`status`が空白のみの場合、無効'
+    context '`status`が10文字の場合、有効'
+    context '`status`が11文字の場合、無効'
+    context '`status`が0文字の場合、無効'
   end
 
   describe '`priority`のバリデーションをテスト' do
-    it '`priority`が10文字の場合、有効'
-    it '`priority`が11文字の場合、無効'
-    it '`priority`が0文字の場合、有効'
+    context '`priority`が10文字の場合、有効'
+    context '`priority`が11文字の場合、無効'
+    context '`priority`が0文字の場合、有効'
   end
 
   describe '`deadline`のバリデーションをテスト' do
-    it '`deadline`が`2023-01-01 17:28:00 UT`の場合、有効'
-    it '`deadline`が`2023-01-01 17:28:00 UT`でない場合、無効'
+    context '`deadline`が`2023-01-01 17:28:00 UT`の場合、有効'
+    context '`deadline`が`2023-01-01 17:28:00 UT`でない場合、無効'
   end
 end
